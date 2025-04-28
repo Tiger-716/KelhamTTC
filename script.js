edbeee29e8fde1f301fecc9f1ef267bf8d33c14f
Tiger-716 <tigerluo716@gmail.com>
2025-04-28 13:03:46 +0100

    deleted json file

--- script.js
+++ script.js
@@ -44,3 +44,40 @@ function updateBooked() {
     }
   }
 }
+
+// https://www.youtube.com/watch?v=cRwpTv33Z_g google sheet
+// https://www.youtube.com/watch?v=XHokFQeQ6Lk json
+
+// fetch('https://tiger-716.github.io/KelhamTTC/timetable.json',
+//   {method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(session)}).then(res => res.json()).then(data => {console.log('succeed');});
+
+
+// get data from google sheet
+const sheetId = "1Yhe8juGUVuoCZiRECcwdjnK36O5DlUfMES8oauzdB0w";
+const sheetName = encodeURIComponent("TimeTable");
+const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=${sheetName}`;
+
+fetch(sheetURL).then((res) => res.text()).then((csvText) => {let sheetObjects = csvToObjects(csvText); console.log(sheetObjects)})
+
+function csvToObjects(csv) {
+  const csvRows = csv.split("\n");
+  const propertyNames = csvSplit(csvRows[0]);
+  let objects = [];
+  for (let i = 1, max = csvRows.length; i < max; i++) {
+    let thisObject = {};
+    let row = csvSplit(csvRows[i]);
+    for (let j = 0, max = row.length; j < max; j++) {
+      thisObject[propertyNames[j]] = row[j];
+    }
+    objects.push(thisObject);
+  }
+  return objects;
+}
+
+function csvSplit(row) {
+  return row.split(",").map((val) => val.substring(1, val.length - 1));
+}
