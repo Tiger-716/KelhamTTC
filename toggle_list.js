d105b8dc2a5ee401ff165fb860b546f4ee54f9c9
Anastasiia Lets <letsnastya@gmail.com>
2025-07-23 13:34:48 +0100

    feat: toggle list of fixtures for a season

---
+++ toggle_list.js
@@ -0,0 +1,11 @@
+var list = document.getElementById("test")
+
+function showFixtureList() {
+    if(list.style.display == "none"){
+        list.style.display = "block";
+    }
+    else{
+        list.style.display = "none";
+    }
+    
+}
